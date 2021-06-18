a287ca83ccd3e0f62714ebb1b7f8dd4d2534fa92
Emaga Alain <emaga87@gmail.com>
2021-06-18 16:09:57 +0100

    Only one post request

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Body, Request, UploadFile, File
 from typing import List, Optional
-from .library.iconverter import CustomImage
+# from .library.iconverter import CustomImage
+import library.iconverter 
 from random import randint
 from pprint import pprint
 import json
@@ -31,22 +32,16 @@ async def upload_file(files: List[UploadFile] = File(...)):
     db.clear()
 
     filenames = []
+    reduced_images = []
 
     for file in files:
         contents = await file.read()
         db.append(contents)
         filenames.append(file.filename)
 
-    return { "filenames": filenames }
+    # return { "filenames": filenames }
 
-
-
-@app.get("/api/images/")
-async def read_random_file(request: Request):
-
-    filenames = json.loads(request.query_params["filenames"])
-
-    reduced_images = []
+    
 
     if len(db) > 0:
 
@@ -65,5 +60,32 @@ async def read_random_file(request: Request):
         return { "converted_images": reduced_images }
     else:
         return {"error": "No image uploaded !!!"}
+
+
+
+# @app.post("/api/images/")
+# async def read_random_file(request: Request):
+
+#     filenames = json.loads(request.query_params["filenames"])
+
+#     reduced_images = []
+
+#     if len(db) > 0:
+
+#         # print(dir(filenames))
+
+#         for index, file in enumerate(filenames):
+
+#             # Convert image
+#             new_image = CustomImage(db[index], file)
+            
+#             response_dic = new_image.reduce_image(size=1, new_quality=50)
+
+#             reduced_images.append({ "image_data": response_dic["img"], "image_name": response_dic["image_filename"] })
+
+
+#         return { "converted_images": reduced_images }
+#     else:
+#         return {"error": "No image uploaded !!!"}
 
 
